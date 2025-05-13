199aa4269f588d5060eb9fef1a5791789c22a4e5
Anul Mehta <anulmehta1848@gmail.com>
2025-05-13 22:03:30 +0530

    13

---
+++ sum.js
@@ -0,0 +1,10 @@
+console.log( "this is calculate sum file");
+
+let name="anul mehta"
+
+function mycalculate(a,b){
+    let sum=a+b;
+    return sum
+}
+
+module.exports={mycalculate,name};
--- xyz.js
+++ xyz.js
@@ -1,2 +1,18 @@
-require("./app.js")
+require("./app.js");
+//this is old way/ normal way of importing any modules
+const obj = require("./sum.js");
+console.log(typeof obj);
 
+console.log(obj.name);
+console.log(obj.mycalculate(10, 20));
+
+//this is object destructing/new way of importing any modules
+const { mycalculate, name } = require("./sum.js");
+
+console.log(name);
+console.log(mycalculate(10, 20));
+
+console.log("this is xyz file ");
+
+const data = require("./data.json");
+console.log(data);
