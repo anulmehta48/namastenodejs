c5d3a2a8fb1f1d0e043efb68c98e6ed60c79f8b1
Anul Mehta <anulmehta1848@gmail.com>
2025-05-16 22:52:23 +0530

    16

--- note.js
+++ note.js
@@ -1,1 +1,3 @@
 //JavaScript=> Java Script is synchronous single threaded language.
+//Javascript is sychronous but using nodejs it become asychronous also because of libuv library it handle all async task
+//so the now we can say javascript is synchronous concurrent asynchronous non-blocking I/O programming langauge.
