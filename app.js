fae2b88b25dfd717b6f9f9097bcace6f829db2f2
Anul Mehta <anulmehta1848@gmail.com>
2025-05-21 22:23:47 +0530

    21

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-console.log("namaste nodejs");
-(function () {
-  let a = 20;
-  console.log(a);
-  console.log("iife");
-})();
+// console.log("namaste nodejs");
+// (function () {
+//   let a = 20;
+//   console.log(a);
+//   console.log("iife");
+// })();
+
+const crypt = require("crypto");
+
+console.log("hello word");
+
+crypt.pbkdf2Sync("password", "salt", 500000, 50, "sha512")
+console.log("First key is generated");
+
+crypt.pbkdf2("password", "salt", 5000, 50, "sha512", (err, key) => {
+  console.log("second key is generated");
+});
+
+function multiplyfn(x, y) {
+  const result = x + y;
+  return result;
+}
+let g = multiplyfn(5, 8);
+console.log(g);
